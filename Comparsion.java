8d4cb9bcd77a9f24b077a2987cb765abd6e4868d
Pradeepkumar Pokala <pradeeppokala90@gmail.com>
2017-03-13 09:52:35 -0400

    Add files via upload

---
+++ Comparsion.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Comparsion 
+{
+public static void main( String args[] )
+{
+	Scanner input = new Scanner( System.in );
+   int num1;
+   int num2;
+   
+   
+  System.out.print("Please compare numbers here\n");
+   
+  System.out.print("enter first integer: ");
+  num1 = input.nextInt();
+  
+  
+  System.out.print("enter second integer:");
+  num2 = input.nextInt();
+  
+
+  
+  if ( num1 < 0  )
+	  
+	  System.out.printf("Given First Number Negitive\n" );
+ if ( num2 < 0  )
+	  
+	  System.out.printf("Given Second Number Negitive\n" );
+ if (num1 < 0 && num2 < 0 )
+  
+	 System.out.printf("Both Numbers are Negitive\n" );
+  if ( num1 == num2 )
+	  System.out.printf("%d ==%d\n", num1, num2 );
+  
+  else {
+  System.out.println("Both Numbers are not equal");
+  
+  }
+  
+  if  (num1 != num2 )
+	  System.out.printf("%d != %d\n", num1,num2);
+  if  (num1 < num2)
+	  System.out.printf("%d < %d\n" , num1,num2);
+  if (num1 > num2 )
+      System.out.printf("%d > %d\n" , num1,num2);
+	
+}
+}
