e5eee016d28e617aa96e8bf6d3aed444ded79cd2
Pradeepkumar Pokala <pradeeppokala90@gmail.com>
2017-03-15 17:26:46 -0400

    Add files via upload
    Removed the statement "both numbers are equal " if given numbers are equal

--- Comparsion.java
+++ Comparsion.java
@@ -20,32 +20,25 @@ public static void main( String args[] )
   
 
   
-  if ( num1 < 0  )
-	  
-	  System.out.printf("Given First Number Negitive\n" );
- if ( num2 < 0  )
-	  
-	  System.out.printf("Given Second Number Negitive\n" );
- 
- if ( num1 == 0)
+	  if ( num1 < 0  )
+		  
+		  System.out.printf("Given First Number Negitive\n" );
+	 if ( num2 < 0  )
+		  
+		  System.out.printf("Given Second Number Negitive\n" );
 	 
-	  System.out.printf("Given First Number has Zero value \n" );
- 
-
-  if ( num1 == num2 )
-	  System.out.printf("%d ==%d\n", num1, num2 );
-  
-  else {
-  System.out.println("Both Numbers are not equal");
-  
-  }
-  
-  if  (num1 != num2 )
-	  System.out.printf("%d != %d\n", num1,num2);
-  if  (num1 < num2)
-	  System.out.printf("%d < %d\n" , num1,num2);
-  if (num1 > num2 )
-      System.out.printf("%d > %d\n" , num1,num2);
+	 
+		 
+	  if ( num1 == num2 )
+		  System.out.printf("%d ==%d\n", num1, num2 );
+	 
+	  if  (num1 != num2 )
+		  System.out.printf("%d != %d\n", num1,num2);
+	  
+	  if  (num1 < num2)
+		  System.out.printf("%d < %d\n" , num1,num2);
+	  if (num1 > num2 )
+	      System.out.printf("%d > %d\n" , num1,num2);
 	
 }
 }
