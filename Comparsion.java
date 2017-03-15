6c5c0faeea230873606f54bdd7afe532ded3f21f
Pradeepkumar Pokala <pradeeppokala90@gmail.com>
2017-03-15 17:39:31 -0400

    Add files via upload
    Added else statement to print both numbers are not equal if not equal

--- Comparsion.java
+++ Comparsion.java
@@ -4,12 +4,9 @@ public class Comparsion
 {
 public static void main( String args[] )
 {
-	Scanner input = new Scanner( System.in );
+    Scanner input = new Scanner( System.in );
    int num1;
    int num2;
-   
-   
-  System.out.print("Please compare numbers here\n");
    
   System.out.print("enter first integer: ");
   num1 = input.nextInt();
@@ -18,27 +15,21 @@ public static void main( String args[] )
   System.out.print("enter second integer:");
   num2 = input.nextInt();
   
-
   
-	  if ( num1 < 0  )
-		  
-		  System.out.printf("Given First Number Negitive\n" );
-	 if ( num2 < 0  )
-		  
-		  System.out.printf("Given Second Number Negitive\n" );
-	 
-	 
-		 
-	  if ( num1 == num2 )
-		  System.out.printf("%d ==%d\n", num1, num2 );
-	 
-	  if  (num1 != num2 )
-		  System.out.printf("%d != %d\n", num1,num2);
-	  
-	  if  (num1 < num2)
-		  System.out.printf("%d < %d\n" , num1,num2);
-	  if (num1 > num2 )
-	      System.out.printf("%d > %d\n" , num1,num2);
-	
+  if ( num1 == num2 )
+      System.out.printf("%d ==%d\n", num1, num2 );
+  else {
+  System.out.println("Both Numbers are not equal");
+  
+  }
+
+ 
+  if  (num1 != num2 )
+      System.out.printf("%d != %d\n", num1,num2);
+  if  (num1 < num2)
+      System.out.printf("%d < %d\n" , num1,num2);
+  if (num1 > num2 )
+      System.out.printf("%d > %d\n" , num1,num2);
+    
 }
 }
